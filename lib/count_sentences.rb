53e60bb8f42248eda1fe66bf2cda241f727f7232
Hal H Nader <halnader89@gmail.com>
2020-06-29 15:54:32 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,14 +7,16 @@ class String
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if do |s|
+       s.length <2
+    end.count
   end
 end
